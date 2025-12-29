a480e34cf5b2426b4b513ded53919e56e602efac
Asad rashid <40260818+AsadRH@users.noreply.github.com>
2025-12-29 10:39:12 +0400

    Update sw.js to cache itself

--- sw.js
+++ sw.js
@@ -1,14 +1,12 @@
 self.addEventListener("install", e => {
   e.waitUntil(
     caches.open("prayer-app").then(cache =>
-      cache.addAll(["index.html", "manifest.json"])
+      cache.addAll([
+        "./",
+        "index.html",
+        "manifest.json",
+        "sw.js"
+      ])
     )
   );
 });
-
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
-});
-
