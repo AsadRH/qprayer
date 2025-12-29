1c536042fd4f2765fd13381da1db661ad7270be2
Asad rashid <40260818+AsadRH@users.noreply.github.com>
2025-12-29 10:24:21 +0400

    Added Tracker files for online and offline both

---
+++ sw.js
@@ -0,0 +1,14 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("prayer-app").then(cache =>
+      cache.addAll(["index.html", "manifest.json"])
+    )
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
+
